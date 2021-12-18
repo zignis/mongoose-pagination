573ed89da6caf4251a6ba4900a66ed236b480766
VelociRaptor <77036902+HexM7@users.noreply.github.com>
2021-12-18 14:57:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,14 +21,14 @@ app.get("/", async (req, res) => {
     page = Number.parseInt(page, 10); //We received the page number as a string, parsing it into an integer
     const totalDocs = await scheme.find().estimatedDocumentCount(); //Fetch total number of documents (.count() method was discontinued, using .estimatedDocumentCount() instead)
     const maxPagination = Math.round(totalDocs / itemsPerPage); //Get the maximum number of possible pages and round to the nearest integer
-    if (page > maxPagination) page = maxPagination; //If the specified page number is greater than the last page index, redirect to the last page
+    if (page > maxPagination) page = maxPagination; //If the specified page number is greater than the last page index, use to the last page instead
   
     const docChunk = await scheme.find().limit(itemsPerPage).skip((itemsPerPage - 1) * 20); 
-    // Fetch the main documents with limit, and skip the documents accroding to the
-    // page number. We use -1 as for the first page we don't have to skip any documents. You can also use .sort() method to sort the documents/
+    // Fetch the main documents under the limit, and skip the documents accroding to the
+    // page number. We use -1 as for the first page we don't have to skip any documents. You can also use .sort() method to sort the documents
     res.send({
       docs: docChunk
-    }); //Finally Send the documents
+    }); //Finally we send the documents
 });
 
 //Express defaults
