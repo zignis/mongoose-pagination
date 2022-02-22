cbdfc002a7f68dbabc8d6981d0aab6e3d6136e19
VelociRaptor <77036902+HexM7@users.noreply.github.com>
2022-02-22 13:46:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,22 +16,27 @@ mongoose.connect(YOUR_MONGO_URL, {
 
 //Using base URL
 app.get("/", async (req, res) => {
-    let { page, sort } = req.query; //Get URL queries
-    if (!page) page=1; //Set the page number to default if not specified in the URL
-    page = Number.parseInt(page, 10); //We received the page number as a string, parsing it into an integer
-    const totalDocs = await scheme.find().estimatedDocumentCount(); //Fetch total number of documents (.count() method was discontinued, using .estimatedDocumentCount() instead)
-    const maxPagination = Math.round(totalDocs / itemsPerPage); //Get the maximum number of possible pages and round to the nearest integer
-    if (page > maxPagination) page = maxPagination; //If the specified page number is greater than the last page index, use to the last page instead
+    let { page, sort } = req.query; // Get URL querie params
+    if (!page) page=1; // Set the page number to 1 (default) if not specified
+    page = Number.parseInt(page, 10);
+    // We received the page number as a string, parsing it into an integer
+    // You may also use Number(page)
+  
+    const totalDocs = await scheme.find().estimatedDocumentCount(); 
+    // Fetch total number of documents (.count() method was discontinued, using .estimatedDocumentCount() instead)
+  
+    const maxPagination = Math.round(totalDocs / itemsPerPage); // Get the maximum number of possible pages and round to the nearest integer
+    if (page > maxPagination) page = maxPagination; // If the specified page number is greater than the last page's index, return the last chunk
   
     const docChunk = await scheme.find().limit(itemsPerPage).skip((itemsPerPage - 1) * 20); 
-    // Fetch the main documents under the limit, and skip the documents accroding to the
+    // Fetch the main documents under the limit, and skip the documents according to the
     // page number. We use -1 as for the first page we don't have to skip any documents. You can also use .sort() method to sort the documents
     res.send({
       docs: docChunk
-    }); //Finally we send the documents
+    }); // Send the documents
 });
 
-//Express defaults
+// Express defaults
 app.listen(YOUR_PORT, () => {
     console.log("Application has started!");
 });
