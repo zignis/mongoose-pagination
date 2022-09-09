753ca417b6910e2849f527c3823ed64460cfc007
VelociRaptor <77036902+HexM7@users.noreply.github.com>
2022-09-09 19:25:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const app = express();
 const mongoose = require('mongoose');
-const scheme = require('./models/sample-scheme.js'); //Path to your data scheme
-const itemsPerPage = 20; //Maximum number of items to display per page
+const scheme = require('./models/sample-scheme.js'); // Path to your data scheme
 
-//Connecting to the database
+const app = express();
+const itemsPerPage = 20; // Records per page
+
+// Connecting to the database
 mongoose.connect(YOUR_MONGO_URL, {        
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -14,26 +15,27 @@ mongoose.connect(YOUR_MONGO_URL, {
   console.log("Unable to connect to the Mongodb database. Error:" + err);
 });
 
-//Using base URL
+// Using root
 app.get("/", async (req, res) => {
-    let { page, sort } = req.query; // Get URL querie params
-    if (!page) page=1; // Set the page number to 1 (default) if not specified
-    page = Number.parseInt(page, 10);
-    // We received the page number as a string, parsing it into an integer
-    // You may also use Number(page)
+    let { page = '1', sort } = req.query; // Get URL query params
+    page = Number.parseInt(page, 10); // Casting to an integer
   
     const totalDocs = await scheme.find().estimatedDocumentCount(); 
     // Fetch total number of documents (.count() method was discontinued, using .estimatedDocumentCount() instead)
   
     const maxPagination = Math.round(totalDocs / itemsPerPage); // Get the maximum number of possible pages and round to the nearest integer
-    if (page > maxPagination) page = maxPagination; // If the specified page number is greater than the last page's index, return the last chunk
+    if (page > maxPagination) page = maxPagination; 
+    // If the specified page number is greater
+    // than the last page's index, return the last chunk
   
     const docChunk = await scheme.find().limit(itemsPerPage).skip((itemsPerPage - 1) * 20); 
-    // Fetch the main documents under the limit, and skip the documents according to the
-    // page number. We use -1 as for the first page we don't have to skip any documents. You can also use .sort() method to sort the documents
-    res.send({
+    // Fetch the documents under the limit, and skip the documents according to the
+    // page number. We use -1 as for the first page we don't have to skip any documents.
+    // You can also use .sort() method to sort the documents
+
+    return res.status(200).json({
       docs: docChunk
-    }); // Send the documents
+    });
 });
 
 // Express defaults
